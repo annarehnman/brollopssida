be816997efacb2a86b6bd1f5ae6dd699d4ad8d95
annarehnman <63710449+annarehnman@users.noreply.github.com>
2021-12-13 15:23:04 +0100

    add sticky navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,26 +1,52 @@
 <template>
-  <nav class="absolute top-0 w-full flex items-center p-5 md:py-8 text-white">
-    <div class="md:w-3/4 mx-auto flex justify-between">
+  <!-- MOBILE -->
+  <nav :class="{ 'navbar-hidden bg-black' : !showNavbar }" class="navbar md:hidden flex justify-between fixed top-0 z-10 w-full p-5 text-white">
+    <!-- brand start -->
+    <div class="text-2xl text-bold">
+      <router-link to="/">Anna & Rikard</router-link>
+    </div>
+    <!-- brand end -->
+    <!-- menu start -->
+    <div class="flex flex-col justify-end">
+      <!-- menu button start -->
+      <div @click="toggleNav" class="flex justify-end mt-2">
+        <button type="button" class="focus:outline-none">
+          <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+            <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+          </svg>
+        </button>
+      </div>
+      <!-- menu button end -->
+
+      <!-- menu list start - open: "block", closed: "hidden" -->
+      <ul :class="showMenu ? 'flex' : 'hidden'" class="flex-col items-end mt-4 space-y-3">
+        <router-link to="/info" @click="toggleNav" class="px-1">Info</router-link>
+        <router-link to="/kontakt" @click="toggleNav" class="px-1">Kontakt</router-link>
+        <button class="border-2 border-white px-4 py-2 text-sm">
+          <router-link to="/kontakt" @click="toggleNav">OSA</router-link>
+        </button>
+      </ul>
+      <!-- menu list end -->
+    </div>
+    <!-- menu end -->
+  </nav>
+
+  <!-- LARGE SCREENS -->
+  <nav class="hidden md:flex items-center absolute top-0 w-full px-5 py-8 text-white">
+    <div class="w-3/4 mx-auto flex justify-between">
       <!-- brand start -->
-      <div class="text-xl md:text-2xl text-bold">
+      <div class="text-2xl text-bold">
         <router-link to="/" @click="toggleNav">Anna & Rikard</router-link>
       </div>
       <!-- brand end -->
       <!-- menu start -->
-        <!-- menu button start -->
-        <div @click="toggleNav" class="md:hidden ml-auto">
-          <button type="button" class="focus:outline-none">
-            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-              <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-            </svg>
-          </button>
-        </div>
-        <!-- menu button end -->
-        <!-- menu list start - open: "block", closed: "hidden" -->
-        <ul :class="showMenu ? 'flex' : 'hidden'" class="flex-col items-center mt-4 space-y-3 md:flex md:flex-row md:mt-0 md:space-y-0 md:space-x-8">
+        <!-- menu list start -->
+        <ul class="flex flex-row items-center mt-0 space-x-8">
           <router-link to="/info" @click="toggleNav" class="px-1 border-b border-transparent hover:border-white active:border-white">Info</router-link>
           <router-link to="/kontakt" @click="toggleNav" class="px-1 border-b border-transparent hover:border-white active:border-white">Kontakt</router-link>
-          <button class="bg-transparent hover:bg-white hover:text-black border-2 border-white px-4 py-2 text-sm"><router-link to="/kontakt" @click="toggleNav">OSA</router-link></button>
+          <button class="bg-transparent hover:bg-white hover:text-black border-2 border-white px-4 py-2 text-sm">
+            <router-link to="/kontakt" @click="toggleNav">OSA</router-link>
+          </button>
         </ul>
         <!-- menu list end -->
       <!-- menu end -->
@@ -33,6 +59,27 @@ import { ref } from 'vue';
 
 export default {
   name: 'Navbar',
+  methods: {
+    onScroll () {
+      const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
+      if (currentScrollPosition < 0) {
+        return
+      }
+      // Stop executing this function if the difference between
+      // current scroll position and last scroll position is less than some offset
+      if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 60) {
+        return
+      }
+      this.showNavbar = currentScrollPosition < this.lastScrollPosition
+      this.lastScrollPosition = currentScrollPosition
+    }
+  },
+  data () {
+    return {
+      showNavbar: true,
+      lastScrollPosition: 0
+    }
+  },
   setup() {
     let showMenu = ref(false);
     const toggleNav = () => (
@@ -40,5 +87,21 @@ export default {
       );
     return { showMenu, toggleNav };
   },
+  mounted () {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.onScroll)
+  }
 };
 </script>
+
+<style scoped>
+.navbar {
+  transform: translate3d(0, 0, 0);
+  transition: 0.3s all ease-out;
+}
+.navbar.navbar-hidden {
+  transform: translate3d(0, -100%, 0);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="bg-home-background bg-cover w-full h-full flex justify-center items-center">
+  <div class="bg-home-background bg-cover w-full h-full flex justify-center items-center relative z-0">
     <div class="md:w-3/4 md:mx-auto p-5 text-white text-center">
-      <div class="space-y-8">
+      <div class="space-y-6 md:space-y-8">
         <h1 class="mb-3 text-5xl md:text-6xl">Anna & Rikard</h1>
         <p class="font-light text-2xl md:text-3xl">30 juli 2022 <span>&#183;</span> Kungsberg Gård, Strängnäs</p>
-        <button class="bg-transparent hover:bg-white hover:text-black border-2 border-white px-6 md:px-7 py-3 md:py-4 text-sm">OSA</button>
+        <button class="bg-transparent hover:bg-white hover:text-black border-2 border-white px-7 py-4 text-sm">OSA</button>
        </div>
+    </div>
+  </div>
+  <div class="bg-white w-full h-full">
+    <div class="md:w-3/4 md:mx-auto p-5">
+      <h1>Content</h1>
     </div>
   </div>
 </template>
