9a20ff476b646bfed0a442ddc92e8e3e88632a6c
annarehnman <63710449+annarehnman@users.noreply.github.com>
2021-12-15 18:02:04 +0100

    add styling about+contact

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,28 +1,46 @@
 <template>
-  <section class="bg-darkbeige bg-cover w-full pt-20 md:pt-24">
+  <section class="bg-home bg-cover h-full w-full flex items-center relative z-0">
     <div class="bg-white w-full flex justify-center">
 
-      <div class="md:w-3/4 px-5 py-10 md:px-0 md:py-20 lg:py-40 text-center lg:text-left text-nearblack">
+      <div class="px-5 py-10 md:px-0 md:py-20 lg:py-40 text-center md:text-left text-nearblack">
+        <p class="text-2xl md:text-4xl font-thin">
+          Info kommer snart!
+        </p>
+      </div>
 
-        <div class="mx-auto lg:flex text-center lg:text-left">
-          <div class="lg:w-1/3 text-4xl md:text-4.5xl font-light">
-            <p>Tid & Plats</p>
+      <div class="hidden md:w-3/4 px-5 py-10 md:px-0 md:py-20 lg:py-40 text-center md:text-left text-nearblack">
+        <div class="md:flex space-x-12 lg:space-x-32">
+          <div class="md:w-1/3 text-4xl lg:text-4.5xl font-light">
+            <p>Schema</p>
           </div>
-          <div class="lg:w-3/4 my-12 lg:mt-0">
+          <div class="md:w-3/4 my-12 md:mt-0">
             <div class="text-xl font-light leading-snug">
               <div class="space-y-3 ">
-                <p>Info kommer här</p>
-                <p>Info kommer här</p>
+                <p>Info kommer här. Info kommer här.</p>
+                <p>Info kommer här. Info kommer här.</p>
+                <p>Info kommer här. Info kommer här.</p>
               </div>
             </div>
           </div>
         </div>
-
-        <div class="mx-auto lg:flex text-center lg:text-left">
-          <div class="lg:w-1/3 text-4xl md:text-4.5xl font-light">
-            <p>Tid & Plats</p>
+        <div class="md:flex space-x-12 lg:space-x-32">
+          <div class="md:w-1/3 text-4xl lg:text-4.5xl font-light">
+            <p>Logistik</p>
           </div>
-          <div class="lg:w-3/4 mt-12 lg:mt-0">
+          <div class="md:w-3/4 my-12 md:mt-0">
+            <div class="text-xl font-light leading-snug">
+              <div class="space-y-3 ">
+                <p>Info kommer här. Info kommer här.</p>
+                <p>Info kommer här. Info kommer här. Info kommer här.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="md:flex space-x-12 lg:space-x-32">
+          <div class="md:w-1/3 text-4xl lg:text-4.5xl font-light">
+            <p>Dresscode</p>
+          </div>
+          <div class="md:w-3/4 mt-12 md:mt-0">
             <div class="text-xl font-light leading-snug">
               <div class="space-y-3 ">
                 <p>Info kommer här</p>
@@ -35,12 +53,6 @@
 
     </div>
   </section>
-
-  <section class="bg-nearblack bg-cover w-full py-20">
-    X
-  </section>
-
-
 </template>
 
 <script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="contact">
-    <h1>Kontakt</h1>
-  </div>
+  <section class="bg-home bg-cover h-full w-full flex items-center relative z-0">
+    <div class="bg-white w-full flex justify-center">
+
+      <div class="px-5 py-10 md:px-0 md:py-20 lg:py-40 text-center md:text-left text-nearblack">
+        <p class="text-2xl md:text-4xl font-thin">
+          Kontakt & OSA kommer snart!
+        </p>
+      </div>
+
+    </div>
+  </section>
 </template>
 
 <script>
