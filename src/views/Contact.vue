b0252f31b135414b10635bd0d3e3e726cc45dad3
annarehnman <63710449+annarehnman@users.noreply.github.com>
2022-01-25 15:52:10 +0100

    updated a tel

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -12,8 +12,8 @@
         <div class="text-xl  leading-snug">
           <p>Anna & Rikard</p>
           <p class="font-thin">Det är vi som gifter oss, känner du inte igen oss vid det här laget är du nog på fel sida.</p>
-          <p class="mt-4"><a href="tel:+731810867">073-181 08 67 (Anna)</a></p>
-          <p><a href="tel:+727229320">072-722 93 20 (Rikard)</a></p>
+          <p class="mt-4"><a href="tel:+46731810867">073-181 08 67 (Anna)</a></p>
+          <p><a href="tel:+46727229320">072-722 93 20 (Rikard)</a></p>
         </div>
       </div>
     </div>
@@ -27,7 +27,7 @@
           <p>Erik Andersson</p>
           <p class="font-thin">Toastmaster och keeper of peace.</p>
           <p class="font-thin">Vill du säga något fint, kul eller insiktsfullt under middagen är det hit du vänder dig.</p>
-          <p class="mt-4"><a href="tel:+737363678">073-736 36 78</a></p>
+          <p class="mt-4"><a href="tel:+46737363678">073-736 36 78</a></p>
           <a href="mailto:apotekareandersson@gmail.com">apotekareandersson@gmail.com</a>             
         </div>
       </div>
@@ -41,7 +41,7 @@
         <div class="text-xl  leading-snug">
           <p>Otilia Bogen</p>
           <p class="font-thin">Brudtärna extra ordinaire, Annas svägerska och partner in crime.</p>
-          <p class="mt-4"><a href="tel:+703595878">070-359 58 78</a></p>
+          <p class="mt-4"><a href="tel:+46703595878">070-359 58 78</a></p>
           <a href="mailto:otilia.bogen@gmail.com">otilia.bogen@gmail.com</a>           
         </div>
       </div>
@@ -55,7 +55,7 @@
         <div class="text-xl  leading-snug">
           <p>Sebastian Opava</p>
           <p class="font-thin">Bestman av rang och betrodd ring-bearer, vän till Rikard sen långt tillbaka.</p>
-          <p class="mt-4"><a href="tel:+735675659">073-567 56 59</a></p>
+          <p class="mt-4"><a href="tel:+46735675659">073-567 56 59</a></p>
           <a href="mailto:sebastian.opava@gmail.com">sebastian.opava@gmail.com</a> 
         </div>
       </div>
