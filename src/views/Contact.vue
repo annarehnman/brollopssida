f535f4a137241438b99c085843916f6e6955aae2
annarehnman <63710449+annarehnman@users.noreply.github.com>
2022-02-25 09:45:46 +0100

    add new images

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,12 +3,10 @@
     <div class="md:w-3/4 mx-auto">
       <h1 class="text-4.5xl text-center lg:text-left">Kontakt</h1>
     </div>
+
     <div class="md:w-3/4 mx-auto lg:flex lg:space-x-32">
       <div class="lg:w-1/3">
-        <!-- IMAGE -->
-        <div class="bg-gray-500 h-72 mb-3 lg:mb-6"></div>
-        <!-- <img src="~@/images/annaorikard.jpg" alt="" class="max-h-min mb-3 lg:mb-6"> -->
-        <!-- IMAGE -->
+        <img src="~@/images/annaochrikardred.jpg" alt="" class="max-h-96 mb-3 lg:mb-6">
       </div>
       <div class="lg:w-3/4 text-xl">
         <p class="font-normal">Anna & Rikard</p>
@@ -17,12 +15,10 @@
         <p><a href="tel:+46727229320" class="text-darkbeige font-normal underline">072-722 93 20</a> (Rikard)</p>
       </div>
     </div>
+
     <div class="md:w-3/4 mx-auto lg:flex lg:space-x-32">
       <div class="lg:w-1/3">
-        <!-- IMAGE -->
-        <div class="bg-gray-500 h-72 mb-3 lg:mb-6"></div>
-        <!-- <img src="~@/images/erik.jpeg" alt="" class="max-h-min mb-3 lg:mb-6"> -->
-        <!-- IMAGE -->
+        <img src="~@/images/erikred.jpg" alt="" class="max-h-96 mb-3 lg:mb-6">
       </div>
       <div class="lg:w-3/4 text-xl">
         <p class="font-normal">Erik Andersson</p>
@@ -31,13 +27,11 @@
         <p class="mt-4"><a href="tel:+46737363678" class="text-darkbeige font-normal underline">073-736 36 78</a></p>
         <a href="mailto:apotekareandersson@gmail.com" class="text-darkbeige font-normal underline">apotekareandersson@gmail.com</a>             
       </div>
+
     </div>
     <div class="md:w-3/4 mx-auto lg:flex lg:space-x-32">
       <div class="lg:w-1/3">
-        <!-- IMAGE -->
-        <div class="bg-gray-500 h-72 mb-3 lg:mb-6"></div>
-        <!-- <img src="~@/images/otilia.jpeg" alt="" class="max-h-min mb-3 lg:mb-6"> -->
-        <!-- IMAGE -->
+        <img src="~@/images/otiliared.jpg" alt="" class="max-h-96 mb-3 lg:mb-6">
       </div>
       <div class="lg:w-3/4 text-xl">
         <p class="font-normal">Otilia Bogen</p>
@@ -46,12 +40,10 @@
         <a href="mailto:otilia.bogen@gmail.com" class="text-darkbeige font-normal underline">otilia.bogen@gmail.com</a>           
       </div>
     </div>
+    
     <div class="md:w-3/4 mx-auto lg:flex lg:space-x-32">
       <div class="lg:w-1/3">
-        <!-- IMAGE -->
-        <div class="bg-gray-500 h-72 mb-3 lg:mb-6"></div>
-        <!-- <img src="~@/images/sebbe.jpg" alt="" class="max-h-min mb-3 lg:mb-6"> -->
-        <!-- IMAGE -->
+        <img src="~@/images/sebbered.jpg" alt="" class="max-h-96 mb-3 lg:mb-6">
       </div>
       <div class="lg:w-3/4 text-xl">
         <p class="font-normal">Sebastian Opava</p>
@@ -60,6 +52,7 @@
         <a href="mailto:sebastian.opava@gmail.com" class="text-darkbeige font-normal underline">sebastian.opava@gmail.com</a> 
       </div>
     </div>
+
   </section>
   <info-section />
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -88,7 +88,7 @@ module.exports = {
     backgroundColor: (theme) => theme('colors'),
     backgroundImage: {
       none: 'none',
-      'poppiesImage': "url('../images/home-bg.jpg')",
+      'poppiesImage': "url('../images/home-bgred.jpg')",
       'gradient-to-t': 'linear-gradient(to top, var(--tw-gradient-stops))',
       'gradient-to-tr': 'linear-gradient(to top right, var(--tw-gradient-stops))',
       'gradient-to-r': 'linear-gradient(to right, var(--tw-gradient-stops))',
